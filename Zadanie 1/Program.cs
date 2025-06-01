297b5eac355550feb70f73d4009fc77bd51e5c19
milenakalwa <milenaakalwa@gmail.com>
2025-06-01 11:52:09 +0200

    Koniec zadań

--- Zadanie 1/Program.cs
+++ Zadanie 1/Program.cs
@@ -8,8 +8,21 @@ namespace Zadanie_1
 {
     internal class Program
     {
+        //Napisz funkcję, która oblicza wartość silni z zadanej przez argument wywołania funkcji
+        //wartości liczby naturalnej.Utwórz wersję rekurencyjną tej funkcji.
+        //Napisz program testowy wykorzystujący napisaną funkcję.
+        static long silnia(int n)
+        {
+            if (n == 0) return 1;
+            else return n * silnia(n - 1);
+        }
         static void Main(string[] args)
         {
+            Console.Write("Podaj n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0) Console.WriteLine("Błędny argument!");
+            else Console.WriteLine("{0}!={1}", n, silnia(n));
+            Console.ReadKey(true);
         }
     }
 }
---
+++ Zadaniee--2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaniee__2
+{
+    internal class Program
+    {
+        /* Napisz funkcję, która wyznacza n-ty wyraz ciągu Fibonacciego zadany przez argument
+        wywołania funkcji wartości liczby naturalnej.Utwórz wersję rekurencyjną tej funkcji.
+        Napisz program testowy wykorzystujący napisaną funkcję */
+        static long fib(long n)
+        {
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            else return fib(n - 1) + fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0) Console.WriteLine("Błędny parametr!");
+            else Console.WriteLine("Fib({0})={1}", n, fib(n));
+            Console.ReadKey(true);
+        }
+    }
+}
